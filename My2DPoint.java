a0bde8b3ad0c97aa4434a0099d0096a2c053bf5e
GodofredoBalcita <48133006+GodofredoBalcita@users.noreply.github.com>
2019-11-26 10:21:34 -0500

    Create My2DPoint.java

---
+++ My2DPoint.java
@@ -0,0 +1,41 @@
+ class My2DPoint {  
+  double x;
+  double  y;
+  
+  static String s = "My2DPoint created";
+  
+  public static void main(String[] args) { 
+    System.out.println(" public static void main was called");
+  }
+  
+// constructor 
+   My2DPoint () {
+     this(3.0,5.0);
+  }
+  
+   My2DPoint (double x, double y) {
+    counter++;
+    this.x = x; this.y = y; 
+    System.out.println(" My2DPoint class called");
+  }
+  
+   double xReflect () {
+    counter++;
+    x = -x; 
+    System.out.println(" x value reflected (My2DPoint) .. ");
+    return x;
+  }
+
+   double xReflect (String a) {
+    counter++;
+    x = -x; 
+    System.out.println(" x value reflected (string a) (My2DPoint) .. ");
+    return x;
+  }
+   
+  public void xPlusy () {
+    x = x+y; 
+    System.out.println(" xplusY  (My2DPoint) .. ");
+  }
+  static int counter;  // same value for all insrances of My2DPoint
+}
